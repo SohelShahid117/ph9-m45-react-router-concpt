79707d903cf3fdfd5a61150596aa2d0fff377772
SohelShahid117 <sohelshahid09@gmail.com>
2024-04-26 22:05:21 +0600

    45-8 Active Route, Loading Spinner, UseLocation, UseNavigation done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "./../Footer/Footer";
+//45-8 Active Route, Loading Spinner, UseLocation, UseNavigation
 
 const Home = () => {
+  const navigation = useNavigation();
+  console.log(navigation);
+  console.log(navigation.state);
+
+  const location = useLocation();
+  console.log(location);
   return (
     <div>
       <Navbar></Navbar>
       {/* <h2>This is home pageeeeeeeeeeeeeee</h2> */}
-      <Outlet></Outlet>
+      {/* {navigation.state === "loading" ? <p>Loading...</p> : ""} */}
+      {navigation.state == "loading" ? <p>Loading...</p> : <Outlet></Outlet>}
+      {/* <Outlet></Outlet> */}
       <Footer></Footer>
     </div>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../Navbar/Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink, useNavigation } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -10,11 +10,16 @@ const Navbar = () => {
         {/* <a href="/">Home</a>
         <a href="/about">About</a>
         <a href="/contact">Contact</a> */}
-        <Link to="/">Home</Link>
+        {/* <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/users">Users</Link>
-        <Link to="/posts">Posts</Link>
+        <Link to="/posts">Posts</Link> */}
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="/posts">Posts</NavLink>
       </nav>
     </div>
   );
